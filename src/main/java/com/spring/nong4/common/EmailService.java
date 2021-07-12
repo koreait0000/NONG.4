ad82e643675be21f3250b5bba3e2cc665dbf4a7d
eofoeofo <durk011@naver.com>
2021-07-12 15:14:24 +0900

    test

--- src/main/java/com/spring/nong4/common/EmailService.java
+++ src/main/java/com/spring/nong4/common/EmailService.java
@@ -3,4 +3,5 @@ package com.spring.nong4.common;
 public interface EmailService {
     void sendSimpleMessage(String to, String subject, String text);
     void sendMimeMessage(String to, String subject, String text);
+    //dsdsds
 }
