79b3b5938556336a2e27576d39aa445cb9800c02
eofoeofo <durk011@naver.com>
2022-01-08 01:04:20 +0900

    220108 OPEN API 이달의 농업기술

--- src/main/webapp/res/js/api/monthFarmTech.js
+++
@@ -1,6 +0,0 @@
-const curationTd = document.querySelector('.curation');
-const curationNo = curationTd.dataset.curationno;
-
-for(let i=0; i<=10; i++){
-    console.log('curationNo : ' + curationNo)
-}
